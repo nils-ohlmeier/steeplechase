803dfbf6c1732dd7661290150b0c9aec8b0b897f
Ted Mielczarek <ted@mielczarek.org>
2013-08-04 07:54:59 -0400

    add devicemanager support

--- steeplechase/runsteeplechase.py
+++ steeplechase/runsteeplechase.py
@@ -2,10 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from mozdevice import DeviceManagerSUT
 from optparse import OptionParser
 from mozprofile import FirefoxProfile, Profile, Preferences
 from mozprofile.permissions import ServerLocations
-from mozrunner import FirefoxRunner, Runner
 from mozhttpd import MozHttpd
 
 import json
@@ -24,12 +24,17 @@ class Options(OptionParser):
         #app path
         #extension path
         #prefs path
+        #remote host IP, port
 
         self.set_usage(usage)
 
 class HTMLTest(object):
-    def __init__(self, test_file):
+    def __init__(self, dm, test_file, test_root, remote_app_path):
+        self.dm = dm
+        self.test_root = test_root
         self.test_file = os.path.abspath(test_file)
+        self.remote_app_path = remote_app_path
+        #XXX: start httpd in main, not here
         self.httpd = MozHttpd(docroot=os.path.dirname(self.test_file))
 
     def run(self):
@@ -49,27 +54,27 @@ class HTMLTest(object):
         for pref in prefs:
           prefs[pref] = Preferences.cast(prefs[pref])
 
-        app_path = "/build/debug-mozilla-central/dist/bin/firefox"
         specialpowers_path = "/build/debug-mozilla-central/dist/xpi-stage/specialpowers"
         with mozfile.TemporaryDirectory() as profile_path:
+            # Create and push profile
             profile = FirefoxProfile(profile=profile_path,
                                      preferences=prefs,
                                      addons=[specialpowers_path],
                                      locations=locations)
+            remote_profile_path = os.path.join(self.test_root, "profile")
+            self.dm.mkDir(remote_profile_path)
+            self.dm.pushDir(profile_path, remote_profile_path)
 
-            env = os.environ.copy()
+            env = {}
             env["MOZ_CRASHREPORTER_NO_REPORT"] = "1"
             env["XPCOM_DEBUG_BREAK"] = "warn"
 
-            cmdargs = [self.httpd.get_url("/" + os.path.basename(self.test_file))]
-            print "cmdargs: %s" % (cmdargs, )
-            runner = FirefoxRunner(profile=profile,
-                                   binary=app_path,
-                                   cmdargs=cmdargs,
-                                   env=env)
-            runner.start()
-            #debug_args=debug_args, interactive=interactive
-            runner.wait()
+            cmd = [self.remote_app_path, "-no-remote",
+                   "-profile", remote_profile_path,
+                   self.httpd.get_url("/" + os.path.basename(self.test_file))]
+            print "cmd: %s" % (cmd, )
+            output = self.dm.shellCheckOutput(" ".join(cmd),
+                                              env = env)
             self.httpd.stop()
         return True
 
@@ -81,11 +86,23 @@ def main(args):
         return 2
 
     log = mozlog.getLogger('steeplechase')
+    dm = DeviceManagerSUT("localhost")
+    # first, push app
+    test_root = dm.getDeviceRoot() + "/steeplechase"
+    if dm.dirExists(test_root):
+        dm.removeDir(test_root)
+    dm.mkDir(test_root)
+    app_path = "/build/debug-mozilla-central/dist/firefox/firefox"
+    remote_app_dir = test_root + "/app"
+    dm.mkDir(remote_app_dir)
+    dm.pushDir(os.path.dirname(app_path), remote_app_dir)
+    remote_app_path = remote_app_dir + "/" + os.path.basename(app_path)
+
     result = True
     for arg in args:
         test = None
         if arg.endswith(".html"):
-            test = HTMLTest(arg)
+            test = HTMLTest(dm, arg, test_root, remote_app_path)
         else:
             #TODO: support C++ tests
             log.error("Unknown test type: %s", arg)
